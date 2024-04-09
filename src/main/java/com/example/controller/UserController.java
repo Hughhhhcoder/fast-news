2542d3d99193ac0cfa19b9760f82ea91dee536a5
“Hugh” <hughz926@gmail.com>
2024-04-09 11:11:09 +0800

    redis登录优化

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -8,12 +8,15 @@ import com.example.utils.ThreadLocalUtil;
 import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.URL;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 
 @RestController
@@ -23,6 +26,8 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
     @PostMapping("/register")
     public Result register(@Pattern(regexp = "^\\S{5,16}$") String username,@Pattern(regexp = "^\\S{5,16}") String password){
         if (userService.findByUserName(username)!=null){
@@ -44,6 +49,9 @@ public class UserController {
             claims.put("id",loginUser.getId());
             claims.put("username",loginUser.getUsername());
             String token = JwtUtil.genToken(claims);
+            //把token存储到redis中
+            ValueOperations<String, String> operations = stringRedisTemplate.opsForValue();
+            operations.set(token,token, 1,TimeUnit.HOURS);
             return  Result.success(token);
         }
         return Result.error("密码错误");
@@ -76,7 +84,7 @@ public class UserController {
         return Result.success();
     }
     @PatchMapping("updatePwd")
-    public Result update(@RequestBody Map<String,String> params){
+    public Result update(@RequestBody Map<String,String> params,@RequestHeader("Authorization")  String token){
 
         //校验参数
         String oldPwd = params.get("old_pwd");
@@ -98,6 +106,9 @@ public class UserController {
         }
         //调用service更新密
         userService.updatePwd(newPwd);
+        //删除reids中对应的token
+        ValueOperations<String, String> operations = stringRedisTemplate.opsForValue();
+        operations.getOperations().delete(token);
         return Result.success();
     }
 
